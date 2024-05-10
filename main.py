0090f0be13e884f67dbf1226e32c7d3795dd6aed
Paolo Sofia <paolosofia6@gmail.com>
2024-05-10 16:26:41 +0200

    feat: refactor names from kart to piazzola kart

--- main.py
+++ main.py
@@ -25,10 +25,14 @@ status: bool = bool(KartDrawerStatus.STOP.value)
 generator: Generator = np.random.default_rng()
 
 
+def init_dataframe() -> pd.DataFrame:
+    return pd.DataFrame({"Piazzole Kart Sorteggiate": []})
+
+
 def start(state: State, id: str | None) -> None:
     print(f"old start status = {state.status}")
     state.status = bool(KartDrawerStatus.RUNNING.value)
-    state.drawn_karts = pd.DataFrame({"kart_sorteggiati": []})
+    state.drawn_karts = init_dataframe()
     print(f"new start status = {state.status}")
 
 
@@ -38,7 +42,7 @@ def reset(state: State, id: str | None) -> None:
     state.status = bool(KartDrawerStatus.STOP.value)
     print(f"new reset status = {state.status}")
     generator = np.random.default_rng()
-    state.drawn_karts = pd.DataFrame({"kart_sorteggiati": []})
+    state.drawn_karts = init_dataframe()
 
 
 def toggle_status(state: State, id: str | None, payload: dict[str, str | list]) -> None:
@@ -54,7 +58,7 @@ def set_num_karts(state: State, id: str | None, payload: dict[str, str | list]) 
     print(f"payload = {payload}")
 
     state.status = bool(KartDrawerStatus.STOP.value)
-    state.drawn_karts = pd.DataFrame({"kart_sorteggiati": []})
+    state.drawn_karts = init_dataframe()
 
 
 def draw_kart(state: State, id: str | None, payload: dict) -> None:
@@ -65,9 +69,9 @@ def draw_kart(state: State, id: str | None, payload: dict) -> None:
 
     while True:
         drawn_kart: int = generator.integers(low=1, high=state.num_karts + 1, size=None)
-        if drawn_kart not in state.drawn_karts.kart_sorteggiati.tolist():
+        if drawn_kart not in state.drawn_karts["Piazzole Kart Sorteggiate"].tolist():
             state.drawn_karts = pd.concat(
-                [state.drawn_karts, pd.DataFrame(data={"kart_sorteggiati": [drawn_kart]})], axis=0
+                [state.drawn_karts, pd.DataFrame(data={"Piazzole Kart Sorteggiate": [drawn_kart]})], axis=0
             )
 
             if state.drawn_karts.shape[0] == state.num_karts:
@@ -76,12 +80,12 @@ def draw_kart(state: State, id: str | None, payload: dict) -> None:
 
 
 with tgb.Page() as page:
-    tgb.text("# Sorteggio Kart", mode="md")
+    tgb.text("# Sorteggio Piazzole Kart", mode="md")
 
     tgb.number(
         label="numero_kart",
         value="{num_karts}",
-        hover_text="Seleziona il numero di kart da sorteggiare",
+        hover_text="Seleziona il numero di piazzole kart da sorteggiare",
         on_change=set_num_karts,
         # on_action=set_num_karts,
         id="numero_kart_input",
@@ -93,7 +97,7 @@ with tgb.Page() as page:
             on_action=start,
             active="{not status}",
             id="start_button",
-            hover_text="Inizia a sorteggiare i kart",
+            hover_text="Inizia a sorteggiare le piazzole kart",
             # properties={"status": status},
         )
 
@@ -102,7 +106,7 @@ with tgb.Page() as page:
             on_action=draw_kart,
             active="{status}",
             id="sorteggia_button",
-            hover_text="Inizia a sorteggiare i kart",
+            hover_text="Sorteggia la prossima piazzola kart",
         )
 
         tgb.button(
@@ -113,7 +117,7 @@ with tgb.Page() as page:
             hover_text="Resetta sorteggio",
         )
 
-    tgb.table(data="{drawn_karts}", hover_text="Kart gia' sorteggiati")
+    tgb.table(data="{drawn_karts}", hover_text="Piazzole Kart gia' sorteggiate")
 
 gui = Gui(page)
-app = tp.run(gui, title="Sorteggio Kart", debug=False, use_reloader=False, run_server=False)
+app = tp.run(gui, title="Sorteggio Piazzole Kart", debug=False, use_reloader=False, run_server=False)
